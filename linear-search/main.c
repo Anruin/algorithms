940c21b9bec29df1702a383e975b20dec5d692b6
Egor Pristavka <anruin@gmail.com>
2023-11-18 23:32:59 +0500

    Linear search.

--- linear-search/main.c
+++ linear-search/main.c
@@ -8,10 +8,10 @@
  * @details Time Complexity: O(n)
  * @details Input: arr sequence of n numbers arr = {a1, a2, ..., an} and a value v.
  * @details Output: index of the first occurrence of the value in the array or -1 if the value is not found.
- * @details Loop Invariant: At the start of each iteration of the for loop, the subarray arr[0..cur-1] does not contain the value v.
- * @details Initialization: cur = 0 as arr[0..0] is empty.
- * @details Maintenance: Each iteration of the for loop checks if the current element is equal to the value v.
- * @details Termination: The subarray arr[0..cur-1] does not contain the value v.
+ * @details Loop Invariant: At the start of each iteration of the for loop, there is no index k < cur such that arr[k] == v.
+ * @details Initialization: cur = 0 and therefor there is no index k < cur such that arr[k] == v.
+ * @details Maintenance: Each iteration of the loop checks if the current element is equal to v. If it is, the index is returned.
+ * @details Termination: The loop terminates when cur == length. If the loop terminates, there is no index k < cur such that arr[k] == v.
  */
 int linear_search(const int arr[], int length, int v) {
 
@@ -21,9 +21,9 @@ int linear_search(const int arr[], int length, int v) {
 	//     return i
 	// return -1
 
-	for (int i = 0; i < length; i++) {
-		if (arr[i] == v) {
-			return i;
+	for (int cur = 0; cur < length; cur++) {
+		if (arr[cur] == v) {
+			return cur;
 		}
 	}
 
@@ -31,17 +31,17 @@ int linear_search(const int arr[], int length, int v) {
 }
 
 int main() {
-	// The array to be summed.
+	// The array to use.
 	int A[] = {5, 2, 15, 13, 4, 6, 1, 3, 9, 12, 11};
 
 	// Number of elements in the array.
 	int lengthOfA = sizeof(A) / sizeof(int);
 
 	int indexExisting = linear_search(A, lengthOfA, 9);
-	printf_s("\nIndex: %d", indexExisting);
+	printf_s("\nIndex of 9: %d", indexExisting);
 
 	int indexNonExisting = linear_search(A, lengthOfA, 99);
-	printf_s("\nIndex: %d", indexNonExisting);
+	printf_s("\nIndex of 99: %d", indexNonExisting);
 
 	return 0;
 }
